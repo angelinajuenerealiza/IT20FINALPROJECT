de1b2a05ed9174cce233955786cdd39a06220b07
angelinajuenerealiza <angeljuenereal@gmail.com>
2025-12-11 09:41:45 +0800

    Add files via upload
    added a linear searching and insertion sort

---
+++ linearSearch.java
@@ -0,0 +1,39 @@
+
+package com.mycompany.mavenproject1;
+
+// Linear Search Implementation
+public FoodItem linearSearch(String name) {
+    for (FoodItem f : foodLibrary) {
+        if (f.name.equalsIgnoreCase(name)) {
+            return f;
+        }
+    }
+    return null; // not found
+}
+
+// Insertion Sort Implementation (sort by calories)
+public void insertionSortByCalories() {
+    for (int i = 1; i < foodLibrary.size(); i++) {
+        FoodItem key = foodLibrary.get(i);
+        int j = i - 1;
+
+        while (j >= 0 && foodLibrary.get(j).calories > key.calories) {
+            foodLibrary.set(j + 1, foodLibrary.get(j));
+            j--;
+        }
+        foodLibrary.set(j + 1, key);
+    }
+}
+// Insertion Sort Implementation (sort by calories)
+public void insertionSortByCalories() {
+    for (int i = 1; i < foodLibrary.size(); i++) {
+        FoodItem key = foodLibrary.get(i);
+        int j = i - 1;
+
+        while (j >= 0 && foodLibrary.get(j).calories > key.calories) {
+            foodLibrary.set(j + 1, foodLibrary.get(j));
+            j--;
+        }
+        foodLibrary.set(j + 1, key);
+    }
+}
